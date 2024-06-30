58e6c3649b73d268c0138ec0eb1ab21d67541f40
Archanakp2001 <84280566+Archanakp2001@users.noreply.github.com>
2024-06-30 17:07:59 +0530

    Create Colors.js

---
+++ frontend/App/Utils/Colors.js
@@ -0,0 +1,10 @@
+export default {
+    BLACK: '#000',
+    WHITE: '#fff',
+    BACKGROUND: '#FFF8E8',
+    PRIMARY: '#85663C',
+    BORDER: '#464646',
+    TEXT: '#696969',
+    HEADLINE: '#D7D7D7', 
+    CARD: '#F1E8D4',
+}
